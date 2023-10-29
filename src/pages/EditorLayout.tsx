e294db540e6e8030c20880d5b8039b5a126358af
yb6b <seetxx@qq.com>
2023-10-29 14:37:43 +0800

    async fetch /cache/*.json

--- src/pages/EditorLayout.tsx
+++ src/pages/EditorLayout.tsx
@@ -1,9 +1,10 @@
-import {
+import React, {
   ChangeEvent,
   Dispatch,
   useContext,
   useEffect,
   useReducer,
+  useState,
 } from "react";
 import { Button, Flex, Layout, Menu, Space, Typography, Upload } from "antd";
 import {
@@ -36,8 +37,6 @@ import { Uploader } from "../components/Utils";
 import { examples } from "../lib/example";
 import { Compound, Form, Repertoire } from "../lib/data";
 import { preprocessForm, preprocessRepertoire } from "../lib/utils";
-import formRaw from "../cache/form.json";
-import repertoireRaw from "../cache/repertoire.json";
 
 const items: MenuProps["items"] = [
   {
@@ -82,8 +81,10 @@ const exportFile = (config: Config) => {
   const blob = new Blob([fileContent], { type: "text/plain" });
   const a = document.createElement("a");
   a.download = `export.yaml`;
-  a.href = window.URL.createObjectURL(blob);
+  const url = window.URL.createObjectURL(blob);
+  a.href = url;
   a.click();
+  window.URL.revokeObjectURL(url); // 避免内存泄漏
 };
 
 const EditorLayout = () => {
@@ -146,30 +147,53 @@ const EditorLayout = () => {
   );
 };
 
+const _cache = {};
+const fetchJson = async (filename: string) => {
+  if (filename in _cache) {
+    return _cache[filename];
+  }
+  const request = await fetch(`/cache/${filename}.json`);
+  const json = await request.json();
+  _cache[filename] = json;
+  return json;
+};
+
 const Contextualized = () => {
   const { pathname } = useLocation();
   const [_, id] = pathname.split("/");
   const [config, dispatch] = useImmerReducer(configReducer, undefined, () => {
     return JSON.parse(localStorage.getItem(id)!) as Config;
   });
-  // const [r, f] = useLoaderData() as [any[], any[]];
-  const repertoire: Repertoire = preprocessRepertoire(repertoireRaw);
-  const form: Form = preprocessForm(formRaw);
+  const [repertoireAndForm, setRepertoire] = useState<[Repertoire, Form]>();
+  useEffect(() => {
+    console.log("读取cache数据");
+    Promise.all([fetchJson("repertoire"), fetchJson("form")]).then(
+      ([repertoire, form]) => {
+        setRepertoire([preprocessRepertoire(repertoire), preprocessForm(form)]);
+      },
+    );
+  }, []);
 
   useEffect(() => {
     localStorage.setItem(id, JSON.stringify(config));
   }, [config, id]);
 
   return (
-    <ConfigContext.Provider value={config}>
-      <DispatchContext.Provider value={dispatch}>
-        <RepertoireContext.Provider value={repertoire}>
-          <FormContext.Provider value={form}>
-            <EditorLayout />
-          </FormContext.Provider>
-        </RepertoireContext.Provider>
-      </DispatchContext.Provider>
-    </ConfigContext.Provider>
+    <>
+      {repertoireAndForm !== undefined ? (
+        <ConfigContext.Provider value={config}>
+          <DispatchContext.Provider value={dispatch}>
+            <RepertoireContext.Provider value={repertoireAndForm[0]}>
+              <FormContext.Provider value={repertoireAndForm[1]}>
+                <EditorLayout />
+              </FormContext.Provider>
+            </RepertoireContext.Provider>
+          </DispatchContext.Provider>
+        </ConfigContext.Provider>
+      ) : (
+        <h3>loading...</h3>
+      )}
+    </>
   );
 };
 
