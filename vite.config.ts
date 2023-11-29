86b00bd2958c89fb2e4fc945ab4e10dca776f25a
yb6b <seetxx@qq.com>
2023-11-29 16:02:54 +0800

    manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import yaml from "@modyfi/vite-plugin-yaml";
 import { importToCDN, autoComplete } from "vite-plugin-external-cdn";
+import { visualizer } from "rollup-plugin-visualizer";
 import Pages from "vite-plugin-pages";
 
 export default defineConfig(({ mode }) => {
@@ -16,9 +17,15 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
+      chunkSizeWarningLimit: 700,
       outDir: `dist/${mode.toLowerCase()}`,
       emptyOutDir: true,
       reportCompressedSize: false,
+      rollupOptions: {
+        output: {
+          manualChunks: manualChunksHandler,
+        },
+      },
     },
     define: {
       APP_VERSION: JSON.stringify(process.env.npm_package_version),
@@ -33,6 +40,16 @@ export default defineConfig(({ mode }) => {
       },
     },
   };
+
+  if (mode === "BEX") {
+    sharedConfig.plugins.push(
+      visualizer({
+        brotliSize: true,
+        title: "打包产物分析",
+        filename: "dist/visualizer.html",
+      }),
+    );
+  }
 
   if (mode === "PAGES") {
     sharedConfig.plugins?.push(
@@ -73,3 +90,38 @@ export default defineConfig(({ mode }) => {
 
   return sharedConfig;
 });
+
+function manualChunksHandler(id: string) {
+  const nodeModulesIndex = id.indexOf("node_modules/");
+  if (nodeModulesIndex === -1) return;
+  const modulePathName = id.slice(nodeModulesIndex + 13);
+  /**
+   * moduleName 大约是这样：
+   *   mathjs
+   *   antd
+   */
+  const moduleName = modulePathName.slice(0, modulePathName.indexOf("/"));
+
+  const patterns = `
+  rc- async-validator
+  ant babel dayjs emotion toggle-selection @ctrl classnames csstype copy-to-clipboard
+  mathjs underscore decimal fraction complex escape-latex seedrandom tiny-emitter typed-function
+  dnd
+  yaml
+  reactflow dagrejs d3- zustand classcat
+  redux
+  react immer redux tslib scheduler
+  `;
+  for (let patternsLine of patterns.split("\n")) {
+    patternsLine = patternsLine.trim();
+    if (!patternsLine) continue;
+    const line = patternsLine.split(" ");
+    const ret = line[0];
+    for (let pat of line) {
+      pat = pat.trim();
+      if (!pat) continue;
+      if (moduleName.includes(pat)) return ret;
+    }
+  }
+  return "vendor";
+}
