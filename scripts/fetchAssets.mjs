2cec05cbed69f626e877e00f49ec8ed11ff59beb
Songchen Tan <i@tansongchen.com>
2023-12-24 22:24:00 -0500

    Fix download issue

---
+++ scripts/fetchAssets.mjs
@@ -0,0 +1,31 @@
+import { writeFileSync, mkdirSync, createWriteStream } from "node:fs";
+import axios from "axios";
+
+const endpoint = "https://assets.chaifen.app/";
+const outputFolder = "public/cache";
+mkdirSync(outputFolder, { recursive: true });
+
+const processFile = async (filename) => {
+  const url = `${endpoint}${filename}.txt`;
+  const jsonPath = `${outputFolder}/${filename}.json`;
+  const response = await axios.get(url);
+  const content = response.data;
+  const tsv = content
+    .trim()
+    .split("\n")
+    .map((x) => {
+      const [key, value] = x.split("\t");
+      return [key, Number(value)];
+    });
+  const object = Object.fromEntries(tsv);
+  writeFileSync(jsonPath, JSON.stringify(object));
+};
+
+for (const name of [
+  "character_frequency",
+  "word_frequency",
+  "key_equivalence",
+  "pair_equivalence",
+]) {
+  await processFile(name);
+}
